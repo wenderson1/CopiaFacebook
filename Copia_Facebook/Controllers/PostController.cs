785bca4b54fed857987daca0cc2524cae743a28b
Wenderson Farias <wenderson.farias10@gmail.com>
2021-12-21 22:18:00 -0300

    Terminado de definir os Endpoints

---
+++ Copia_Facebook/Controllers/PostController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Copia_Facebook.API.Controllers
+{
+    [Route("api/posts")]
+    public class PostController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get(string query)
+        {
+            return NoContent();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            return NoContent();
+        }
+
+        [HttpGet("getPostsActive")]
+        public IActionResult GetPostsActive(string query)
+        {
+            return NoContent();
+        }
+
+        [HttpGet("getPostsDeactive")]
+        public IActionResult GetPostsDeactive(string query)
+        {
+            return NoContent();
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Post post)
+        {
+            return NoContent();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put([FromBody] Post post)
+        {
+            return NoContent();
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            return NoContent();
+        }
+        
+        
+    }
+}
---
+++ Copia_Facebook/Controllers/UserController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Copia_Facebook.API.Controllers
+{
+    [Route("api/users")]
+    public class UserController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult Get(string query)
+        {
+            return NoContent();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            return NoContent();
+        }
+
+        [HttpGet("usersActive")]
+        public IActionResult GetUsersActive(string query)
+        {
+            return NoContent();
+        }
+
+        [HttpGet("usersDeactive")]
+        public IActionResult GetUsersDeactive(string query)
+        {
+            return NoContent();
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] User user)
+        {
+            return NoContent();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody]User user)
+        {
+            return NoContent();
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            return NoContent();
+        }
+    }
+}
