d8b1051e51fd7b9133a6d2e6a83e2e9c6712aab1
Wenderson Farias <wenderson.farias10@gmail.com>
2021-12-22 00:13:19 -0300

    terminado ef core

--- Copia_Facebook/Startup.cs
+++ Copia_Facebook/Startup.cs
@@ -1,7 +1,9 @@
+using CopiaFacebook.Infrastructure.Persistence;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,7 +28,8 @@ namespace Copia_Facebook
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddDbContext<CopiaFacebookDbContext>(opt => opt.UseInMemoryDatabase("CopiaFacebookDatabase"));
+            services.AddScoped<CopiaFacebookDbContext, CopiaFacebookDbContext>();
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
